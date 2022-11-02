e719e523aeec8bb64bd257797cff6dc9431eca21
Safa Zgolli <fifi.zgolli89@gmail.com>
2022-11-01 22:18:04 -0400

    new project link added

--- assets/images/CSS_ Grid Tutorial - Front-End Engineering Curriculum - Turing School of Software and Design_files/active-links.js
+++
@@ -1,8 +0,0 @@
-$('.nav-links a:not(.home-logo-link)').each(function(idx, link) {
-  var currentPath = window.location.pathname;
-  var linkPath = $(link).attr('href');
-
-  if (currentPath.includes(linkPath)) {
-    $(link).toggleClass('active-link');
-  }
-});
